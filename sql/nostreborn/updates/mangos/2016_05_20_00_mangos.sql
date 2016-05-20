bb10f43aff6095b7985711dfc36e08e402216803
Lobus <davidsrowell@yahoo.com>
2016-05-20 22:18:19 +0200

    Create 2016_05_20_00_mangos.sql
    DB/World: Fix despwan of Duskwood Chest

---
+++ sql/nostreborn/updates/mangos/2016_05_20_00_mangos.sql
@@ -0,0 +1,7 @@
+DELETE FROM gameobject WHERE id = 124072;
+ 
+DELETE FROM gameobject_template WHERE entry = 123214;
+INSERT INTO `gameobject_template`
+    (`entry`, `type`, `displayId`, `name`, `faction`, `flags`, `size`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`, `data12`, `data13`, `data14`, `data15`, `data16`, `data17`, `data18`, `data19`, `data20`, `data21`, `data22`, `data23`, `mingold`, `maxgold`, `ScriptName`, `doLoad`)
+VALUES
+    (123214, 3, 1, 'Duskwood Chest', 0, 2, 1, 24, 5044, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 1);
